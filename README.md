48eb484d4390bbed1b96a0214194bb5798721551
Veera Virtanen <veeraeila16@gmail.com>
2025-11-21 22:14:32 +0200

    Todo sovelluksen aloittaminen, tehtävän lisäämisominaisuus, tehtävien listaus ja säilytys sekä README päivitys

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Todo sovellus - Rust Yew
+# Seminaarityö sovellus - Rust Yew
+
+###  Tavoitteet ja toteutus
+Seminaarityöni tavoitteena on luoda yksinkertainen web-sovellus käyttäen Rust ohjelmointikieltä ja Yew sovelluskehystä. Sovellus on "todo" eli tehtävienhallintasovellus, jossa käyttäjä pystyy listaamaan ja seuraamaan omia tehtäviään. Tähän raporttiin tulen kirjaamaan kokemuksiani sovelluksen kehittämisen erivaiheista. 
+
+Olin ennen projektia opiskellut Rust ohjelmointikieltä, mutta Yew oli minulle täysin uusi. Apuna sovelluksen kehittämiseen käytin Yew:n virallista dokumentaatiota sekä virallista Yew:n esimerkki sovellusta todomvc GitHubissa.
+
+Pelkästään dokumentaation avulla oli vaikea päästä alkuun, joten esimerkistä oli paljon hyötyä. Dokumentaatiossakin eri aiheiden kohdissa suositeltiinkin katsomaan joitain virallisia Yew:n esimerkki sovelluksia.
+
+
+### Lähteet
+Klabnik, S., Nichols, C., & Krycho, C. The Rust Programming Language. https://doc.rust-lang.org/book/. Luettu: 20.11.2025
+
+Yew. https://yew.rs. Luettu: 19.11.2025.
+
+Yew Stack. 2025. todomvc. https://github.com/yewstack/yew/tree/master/examples/todomvc. GitHub.
--- yew-app/src/main.rs
+++ yew-app/src/main.rs
@@ -1,9 +1,101 @@
-use yew::prelude::*;
+use gloo::storage::{LocalStorage, Storage};
+use serde::{Deserialize, Serialize};
+use web_sys::HtmlInputElement;
+use yew::{html::Scope, prelude::*};
 
-#[function_component(App)]
-fn app() -> Html {
-    html! {
-        <h1>{ "Todo App" }</h1>
+const KEY: &str = "yew.todo";
+
+#[derive(Properties, PartialEq, Clone, Deserialize, Serialize)]
+struct TodoItem {
+    text: String,
+    #[prop_or(false)]
+    complete: bool,
+}
+
+struct App {
+    todos: Vec<TodoItem>,
+    focus_ref: NodeRef,
+}
+
+enum Msg {
+    Add(String),
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        let todos = LocalStorage::get(KEY).unwrap_or_else(|_| Vec::new());
+        let focus_ref = NodeRef::default();
+        Self { todos, focus_ref }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+        <div>
+            <h2>{ "Todos" }</h2>
+                { self.view_input(ctx.link()) }
+            <ul>
+              { for self
+                .todos
+                .iter()
+                .enumerate()
+                .map(|(i, e)| self.view_todo((i, e), ctx.link()))
+                }
+            </ul>
+        </div>
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Add(text) => {
+                let text = text.trim();
+
+                let todo = TodoItem {
+                    text: text.to_string(),
+                    complete: false,
+                };
+                self.todos.push(todo);
+            }
+        }
+        LocalStorage::set(KEY, &self.todos).expect("failed to set");
+        true
+    }
+}
+
+
+impl App {
+    fn view_input(&self, link: &Scope<Self>) -> Html {
+        let onkeypress = link.batch_callback(|event: KeyboardEvent| {
+            if event.key() == "Enter" {
+                let input: HtmlInputElement = event.target_unchecked_into();
+                let value = input.value();
+                input.set_value("");
+                Some(Msg::Add(value))
+            } else {
+                None
+            }
+        });
+        
+        html! {
+            <div>
+                <input
+                    type="text"
+                    {onkeypress}
+                />
+            </div>
+        }
+    }
+    fn view_todo(&self, (i, todo): (usize, &TodoItem), link: &Scope<Self>) -> Html {
+        html! {
+            <li>
+                <div>
+                    <label>{&todo.text}</label>
+                </div>
+            </li>
+        }
     }
 }
 
